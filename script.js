0e183c9216fdc45521298f3abbc78930dddcc95a
jinethB2006 <jineth200655@gmail.com>
2025-10-07 19:49:28 +0530

    updated

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
-function addweather(){
-    const place=document.getElementById('weather').value;
-    console.log(place);
+async function addweather(){
+    const place=document.getElementById('weather').value.trim();
+    const result=document.getElementById('result');
+    if(place===""){
+        result.innerHTML="<p style='color:red;'>Plese enter the city name</p>";
+        return;
+    }
+    const apikey="375add281775bdaec8244a0cc34bc9da";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apiKey}&units=metric`;
+    
 }
