0897b67b627fbb415501836ea3b9e49374a00dc9
jinethB2006 <jineth200655@gmail.com>
2025-10-08 17:54:04 +0530

    uptaded css and js

--- script.js
+++ script.js
@@ -7,6 +7,23 @@ async function addweather(){
         return;
     }
     const apikey="375add281775bdaec8244a0cc34bc9da";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apiKey}&units=metric`;
-    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apikey}&units=metric`;
+    try{
+        const responce = await fetch(url);
+        if (!responce.ok){
+            throw new Error("City not found");
+        }
+        const data=await responce.json();
+
+        const cityname=data.name;
+        const country=data.sys.country;
+        const temperature=data.main.temp;
+        const weather= data.weather[0].description;
+
+        result.innerHTML=`<h3>${cityname},${country}</h3>
+        <p>Temperature: ${temperature}C</p>
+        <p>Condition: ${weather}</p>`;
+    }catch(error){
+        result.innerHTML=`<p style="color:red;">${error.message}</p>`;
+    }
 }
